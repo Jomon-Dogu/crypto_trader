d292d8b41c99176f6b12841aee211ed0b8a87b85
Andreas <a-wolff-kastiel@protonmail.com>
2024-11-30 22:07:40 +0100

    change main absolute PATH

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from key_manager import APIKeyManager
-from crypto_read import CryptoPriceAnalyzerCoinbase
-from crypto_read_alternativ import CryptoPriceAnalyzerCryptoCompare
+from crypto_viewer.key_manager import APIKeyManager
+from crypto_viewer.crypto_read import CryptoPriceAnalyzerCoinbase
+from crypto_viewer.crypto_read_alternativ import CryptoPriceAnalyzerCryptoCompare
+
 import time
 
 
